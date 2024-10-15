656a925ff611e4cb6a047bdd0d0109637a08e077
Juandoqg <juandoqg@gmail.com>
2024-10-15 11:27:24 -0500

    altero modelo usuario

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # Tus propiedades personalizadas
+    documento = models.IntegerField(null= True)
+    expedicion = models.DateField(null=True)
     is_chef = models.BooleanField(default=False)
     is_waiter = models.BooleanField(default=False)
 
