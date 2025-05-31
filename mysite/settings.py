58538cc79c6dc7e68644dd0d63b955f08c2b31b4
Juandoqg <juandoqg@gmail.com>
2025-05-31 18:23:47 -0500

    crsf

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'myapp.User'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://restaurant-production-d84b.up.railway.app',
+    'restaurant-production-7e9d.up.railway.app',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
